40b23610970699aeeb3b02dff30b5830a8b5e67c
Michiel Leyman <michiel.leyman@gmail.com>
2021-10-22 14:06:36 +0200

    refactor: create auth store utility

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { session } from '$app/stores';
+	import { user } from '$lib/stores/auth';
 </script>
 
-{#if $session.user}
-	<h1>You're logged in! {$session.user.email}</h1>
+{#if $user}
+	<h1>You're logged in! {$user.email}</h1>
 {:else}
 	<h1>You're not logged in</h1>
 	<a href="/auth/login">Log in now</a>
